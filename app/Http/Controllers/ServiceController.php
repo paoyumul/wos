2f354f24b2e190737a28acf2a663d837431d6c82
Erwin Paolo Yumul <paoyumul@gmail.com>
2022-06-26 13:08:46 +0800

    Update ServiceController.php

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -32,6 +32,11 @@ class ServiceController extends Controller
     {
         $this->authorize('create', Service::class);
 
+        $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|numeric',
+        ]);
+        
         Service::create($request->all());
 
         session()->flash('message', 'The service has been created.');
