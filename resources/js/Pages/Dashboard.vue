aa45d42d77601011f758d2a3d92042644a15ddf4
Erwin Paolo Yumul <paoyumul@gmail.com>
2022-07-03 15:56:52 +0800

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,7 +3,7 @@
         <template v-slot:pageTitle></template>
     </PageHeader>
     <div class="grid grid-cols-12">
-        <div class="col-span-6">
+        <div class="col-span-6 md:col-span-12">
             <div class="text-lg mb-2" v-if="upcomingAppointments.length">Upcoming Appointments</div>
             <table v-if="upcomingAppointments.length">
                 <thead>
